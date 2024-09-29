a5c42b218d7e46821306ef2bf7417ec944156498
fyInALT <fy@altresear.ch>
2024-09-29 15:41:28 +0800

    from babylonio fg

--- finality-gadget/sdk/client/expected_clients.go
+++ finality-gadget/sdk/client/expected_clients.go
@@ -1,7 +1,7 @@
 package client
 
 import (
-	"github.com/alt-research/blitz/finality-gadget/sdk/cwclient"
+	"github.com/babylonlabs-io/finality-gadget/types"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 )
@@ -10,6 +10,7 @@ type IBabylonClient interface {
 	QueryAllFpBtcPubKeys(consumerId string) ([]string, error)
 	QueryFpPower(fpPubkeyHex string, btcHeight uint64) (uint64, error)
 	QueryMultiFpPower(fpPubkeyHexList []string, btcHeight uint64) (map[string]uint64, error)
+	QueryEarliestActiveDelBtcHeight(fpPubkeyHexList []string) (uint64, error)
 }
 
 type IBitcoinClient interface {
@@ -17,10 +18,11 @@ type IBitcoinClient interface {
 	GetBlockHashByHeight(height uint64) (*chainhash.Hash, error)
 	GetBlockHeaderByHash(blockHash *chainhash.Hash) (*wire.BlockHeader, error)
 	GetBlockHeightByTimestamp(targetTimestamp uint64) (uint64, error)
+	GetBlockTimestampByHeight(height uint64) (uint64, error)
 }
 
 type ICosmWasmClient interface {
-	QueryListOfVotedFinalityProviders(queryParams *cwclient.L2Block) ([]string, error)
+	QueryListOfVotedFinalityProviders(queryParams *types.Block) ([]string, error)
 	QueryConsumerId() (string, error)
 	QueryIsEnabled() (bool, error)
 }
