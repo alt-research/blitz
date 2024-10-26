ea36476f5e52df7808244209e43ea66136836b88
fyInALT <97101459+fyInALT@users.noreply.github.com>
2024-10-26 16:15:14 +0800

    feat: delete useless codes (#22)
    * delete useless codes
    
    * rename fp mod

--- finality-gadget/operator/cmd/keys.go
+++ finality-gadget/operator/cmd/keys.go
@@ -7,13 +7,11 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 
 	"github.com/alt-research/blitz/finality-gadget/core/logging"
 	"github.com/alt-research/blitz/finality-gadget/core/utils"
 	"github.com/alt-research/blitz/finality-gadget/operator/configs"
-	"github.com/alt-research/blitz/finality-gadget/operator/finalityprovider"
 )
 
 func keysRestore(cliCtx *cli.Context) error {
@@ -35,28 +33,29 @@ func keysRestore(cliCtx *cli.Context) error {
 
 	logger.Debug("key restore", "name", keyName, "mnemonic", mnemonic)
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	_, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	cp, err := finalityprovider.NewProvider(ctx, &config.FinalityProvider, logger.Inner())
-	if err != nil {
-		return errors.Wrap(err, "new provider failed")
-	}
+	/*
+		cp, err := finalityprovider.NewProvider(ctx, &config.FinalityProvider, logger.Inner())
+		if err != nil {
+			return errors.Wrap(err, "new provider failed")
+		}
 
-	if cp.KeyExists(keyName) {
-		return errors.Errorf("the key %s already exists", keyName)
-	}
+		if cp.KeyExists(keyName) {
+			return errors.Errorf("the key %s already exists", keyName)
+		}
 
-	// TODO: use flag
-	coinType := 118
+		// TODO: use flag
+		coinType := 118
 
-	address, err := cp.RestoreKey(keyName, mnemonic, uint32(coinType), cp.PCfg.SigningAlgorithm)
-	if err != nil {
-		return err
-	}
+		address, err := cp.RestoreKey(keyName, mnemonic, uint32(coinType), cp.PCfg.SigningAlgorithm)
+		if err != nil {
+			return err
+		}
 
-	logger.Info("restore key", "address", address)
-
+		logger.Info("restore key", "address", address)
+	*/
 	return nil
 }
 
@@ -68,7 +67,7 @@ func keysShow(cliCtx *cli.Context) error {
 	}
 	config.WithEnv()
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	_, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	logger, err := logging.NewZapLogger(logging.NewLogLevel(config.Common.Production))
@@ -81,24 +80,26 @@ func keysShow(cliCtx *cli.Context) error {
 
 	logger.Info("show key", "name", keyName)
 
-	cp, err := finalityprovider.NewProvider(ctx, &config.FinalityProvider, logger.Inner())
-	if err != nil {
-		return errors.Wrap(err, "new provider failed")
-	}
+	/*
+		cp, err := finalityprovider.NewProvider(ctx, &config.FinalityProvider, logger.Inner())
+		if err != nil {
+			return errors.Wrap(err, "new provider failed")
+		}
 
-	keyStore, err := cp.Keybase.Key(keyName)
-	logger.Info("keystore", "store", keyStore, "err", err)
+		keyStore, err := cp.Keybase.Key(keyName)
+		logger.Info("keystore", "store", keyStore, "err", err)
 
-	if !cp.KeyExists(keyName) {
-		return errors.Errorf("the key %s no exists", keyName)
-	}
+		if !cp.KeyExists(keyName) {
+			return errors.Errorf("the key %s no exists", keyName)
+		}
 
-	key, err := cp.GetKeyAddressForKey(keyName)
-	if err != nil {
-		return errors.Wrapf(err, "failed to get key address for %v", keyName)
-	}
+		key, err := cp.GetKeyAddressForKey(keyName)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get key address for %v", keyName)
+		}
 
-	logger.Debug("key address", "name", keyName, "address", key)
+		logger.Debug("key address", "name", keyName, "address", key)
+	*/
 
 	return nil
 }
--- finality-gadget/operator/configs/operator_config.go
+++ finality-gadget/operator/configs/operator_config.go
@@ -1,21 +1,29 @@
 package configs
 
 import (
+	"cosmossdk.io/errors"
 	"github.com/alt-research/blitz/finality-gadget/client/eotsmanager"
 	"github.com/alt-research/blitz/finality-gadget/client/l2eth"
 	"github.com/alt-research/blitz/finality-gadget/core/configs"
 	commonConfig "github.com/alt-research/blitz/finality-gadget/core/configs"
 	"github.com/alt-research/blitz/finality-gadget/core/utils"
-	"github.com/alt-research/blitz/finality-gadget/operator/finalityprovider"
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 type OperatorConfig struct {
-	Common                   commonConfig.CommonConfig `yaml:"common,omitempty"`
-	Layer2                   l2eth.Config              `yaml:"layer2,omitempty"`
-	Babylon                  configs.BabylonConfig     `yaml:"babylon,omitempty"`
-	FinalityProvider         finalityprovider.Config   `yaml:"finalityProvider,omitempty"`
-	EOTSManagerConfig        eotsmanager.Config        `yaml:"eotsManager,omitempty"`
-	FinalityProviderHomePath string                    `yaml:"finalityProviderHomePath,omitempty"`
+	Common            commonConfig.CommonConfig `yaml:"common,omitempty"`
+	Layer2            l2eth.Config              `yaml:"layer2,omitempty"`
+	Babylon           configs.BabylonConfig     `yaml:"babylon,omitempty"`
+	EOTSManagerConfig eotsmanager.Config        `yaml:"eotsManager,omitempty"`
+
+	// fp home root path create by fpd.
+	FinalityProviderHomePath string `yaml:"finalityProviderHomePath,omitempty"`
+	// fp_addr is the bech32 chain address identifier of the finality provider.
+	FpAddr string `yaml:"fp_addr,omitempty"`
+	// btc_pk is the BTC secp256k1 PK of the finality provider encoded in BIP-340 spec
+	BtcPk string `yaml:"btc_pk,omitempty"`
 }
 
 // use the env config first for some keys
@@ -23,8 +31,24 @@ func (c *OperatorConfig) WithEnv() {
 	c.Common.WithEnv()
 	c.Layer2.WithEnv()
 	c.Babylon.WithEnv()
-	c.FinalityProvider.WithEnv()
 	c.EOTSManagerConfig.WithEnv()
 
 	c.FinalityProviderHomePath = utils.LookupEnvStr("FINALITY_PROVIDER_HOME_PATH", c.FinalityProviderHomePath)
+	c.FpAddr = utils.LookupEnvStr("FINALITY_PROVIDER_ADDRESS", c.FpAddr)
+	c.BtcPk = utils.LookupEnvStr("FINALITY_PROVIDER_BTC_PK", c.BtcPk)
+
+}
+
+func (c *OperatorConfig) GetBtcPk() (*btcec.PublicKey, error) {
+	btcPkBytes, err := hexutil.Decode(c.BtcPk)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid BTC public key hex string: %v", c.BtcPk)
+	}
+
+	btcPk, err := schnorr.ParsePubKey(btcPkBytes)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid BTC public key: %w", c.BtcPk)
+	}
+
+	return btcPk, nil
 }
