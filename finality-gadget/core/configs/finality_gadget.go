9c63dfff3387969f3806a1a413dc9ca1a52a512d
fyInALT <97101459+fyInALT@users.noreply.github.com>
2024-12-18 21:40:16 +0800

    feat: delete useless configs (#37)
    * delete useless codes
    
    * fix lint

--- finality-gadget/core/configs/finality_gadget.go
+++ finality-gadget/core/configs/finality_gadget.go
@@ -2,19 +2,20 @@ package configs
 
 import (
 	"github.com/alt-research/blitz/finality-gadget/core/utils"
-	finalityGadgetConfig "github.com/babylonlabs-io/finality-gadget/config"
 )
 
+type Config struct {
+	BBNChainID    string `yaml:"bbnchainid" description:"BabylonChain chain ID"`
+	BBNRPCAddress string `yaml:"bbnrpcaddress" description:"BabylonChain chain RPC address"`
+	DBFilePath    string `yaml:"dbfilepath" description:"path to the DB file"`
+}
+
 type BabylonConfig struct {
-	FinalityGadgetCfg finalityGadgetConfig.Config `yaml:"finality_gadget"`
+	FinalityGadgetCfg Config `yaml:"finality_gadget"`
 }
 
 func (c *BabylonConfig) WithEnv() {
 	c.FinalityGadgetCfg.DBFilePath = utils.LookupEnvStr("FINALITY_GADGET_DB_FILE_PATH", c.FinalityGadgetCfg.DBFilePath)
 	c.FinalityGadgetCfg.BBNChainID = utils.LookupEnvStr("FINALITY_GADGET_BABYLON_CHAINID", c.FinalityGadgetCfg.BBNChainID)
 	c.FinalityGadgetCfg.BBNRPCAddress = utils.LookupEnvStr("FINALITY_GADGET_BABYLON_RPC_ADDR", c.FinalityGadgetCfg.BBNRPCAddress)
-}
-
-func (c *BabylonConfig) FinalityGadget() *finalityGadgetConfig.Config {
-	return &c.FinalityGadgetCfg
 }
